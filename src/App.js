e071f5137045e6bb436a3aeeb49aa0b0e42a9471
Graeme Doherty <graeme.doherty@gmail.com>
2024-06-25 20:51:34 +0100

    add text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className='App'>Test</div>;
+  return (
+    <div className='App'>
+      <h1>Test</h1>
+    </div>
+  );
 }
 
 export default App;
